b1836be94dfd16658f75f77c66c8aa316207a9ac
varun nagar <varun.nagar@ust.com>
2025-04-27 00:30:05 +0530

    implemented landing page with API data from DB

--- apps/main_portal/src/landing/landing.component.ts
+++ apps/main_portal/src/landing/landing.component.ts
@@ -3,21 +3,24 @@ import { CommonModule } from '@angular/common';
 
 import { ButtonModule } from 'primeng/button';
 import { TableModule } from 'primeng/table';
+import { TagModule } from 'primeng/tag';
 import { AccountService } from '../service/account.service';
 
 @Component({
   selector: 'app-landing',
-  imports: [CommonModule, ButtonModule, TableModule],
+  imports: [CommonModule, ButtonModule, TableModule, TagModule],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css',
 })
 export class LandingComponent implements OnInit {
 
   cols: any[] = [
-    { field: 'code', header: 'Code' },
     { field: 'name', header: 'Name' },
-    { field: 'category', header: 'Category' },
-    { field: 'quantity', header: 'Quantity' }
+    { field: 'status', header: 'Status' },
+    { field: 'address', header: 'City', subfield: 'city' },
+    { field: 'address', header: 'Pin Code', subfield: 'pincode' },
+    { field: 'sensor_count', header: 'Total Sensor' },
+    { field: 'actions', header: 'Action' }
   ];
 
   accounts: any[] = [];
@@ -28,6 +31,10 @@ export class LandingComponent implements OnInit {
     this.accountService.getAccounts().subscribe(data=>{
       this.accounts = data;
     })
+  }
+
+  editAccount(account: any) {
+    console.log('Edit account:', account);
   }
 
 }
